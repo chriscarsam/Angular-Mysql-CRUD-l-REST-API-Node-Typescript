da14ef0315a0342309a33d0fbae3dbd897f48a4d
Chris <chriscar.sam@gmail.com>
2019-04-21 21:19:13 -0500

    formulario de ingreso de datos

--- client/src/app/components/game-form/game-form.component.ts
+++ client/src/app/components/game-form/game-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Game } from 'src/app/models/Game';
+
+import {  GamesService } from '../../services/games.service';
 
 @Component({
   selector: 'app-game-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameFormComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') classes ='row';
+
+  game: Game = {
+    id: 0,
+    title: '',
+    description: '',
+    image: '',
+    created_at: new Date()
+  }
+
+  constructor(private gamesService: GamesService) { }
 
   ngOnInit() {
   }
 
+  saveNewGame(){
+    delete this.game.created_at;
+    delete this.game.id;
+    
+    this.gamesService.saveGame(this.game)
+    .subscribe(
+      res => {
+        console.log(res);
+      },
+      err => console.error(err)
+    )
+  }
 }
